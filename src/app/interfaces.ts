1c44d35ca7f865819fe37b2f92780dfe83c48da8
Lionel <lionel@morrison.mobi>
2023-08-21 10:01:55 -0400

    Init nav

---
+++ src/app/interfaces.ts
@@ -0,0 +1,10 @@
+export interface NavLinks {
+  [label: string]: NavLink;
+}
+
+export interface NavLink {
+  href: string;
+  target?: '_blank' | string;
+  disabled?: boolean;
+  cssClass?: string;
+}
