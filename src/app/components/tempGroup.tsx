0cca3077a254ebb1cecb5b66992260fde8fd59e2
Lionel <lionel@morrison.mobi>
2023-08-25 13:10:43 -0400

    Implemented Suspense and Loading

--- src/app/components/tempGroup.tsx
+++ src/app/components/tempGroup.tsx
@@ -1,9 +1,10 @@
 'use client';
 import { DeviceStatus, LambdaDeviceStatus, Preferences } from '../interfaces';
 import { SwitchBotDevices } from '../switchBotDevices';
-import { useEffect, useState } from 'react';
+import React, { Suspense, useEffect, useState } from 'react';
 import TempWidget from './temp';
 import useWebSocket from 'react-use-websocket';
+import Loading from './loading';
 
 export default function TempGroupWidget({
   urls,
@@ -12,48 +13,49 @@ export default function TempGroupWidget({
   urls: { ws: string; lambda: string };
   prefs: Preferences;
 }) {
-  const [isLoading, setIsLoading] = useState(true);
   const [loadedDevices, setLoadedDevices] = useState<DeviceStatus[]>([]);
 
   const accountId = 'HCC';
   const SBDevices = new SwitchBotDevices(urls.lambda, accountId);
 
-    useWebSocket(urls.ws, {
-      onOpen: () => {
-        console.log('WebSocket Established');
-      },
-      onClose: () => {
-        console.log('WebSocket Disconnected');
-      },
-      onMessage: (event) => {
-        const lambdaDevices = JSON.parse(event.data) as LambdaDeviceStatus[];
+  useWebSocket(urls.ws, {
+    onOpen: () => {
+      console.log('WebSocket Established');
+    },
+    onClose: () => {
+      console.log('WebSocket Disconnected');
+    },
+    onMessage: (event) => {
+      const lambdaDevices = JSON.parse(event.data) as LambdaDeviceStatus[];
 
-        setLoadedDevices(
-          SBDevices.transformDeviceStatus(lambdaDevices, SBDevices.getDeviceNames(loadedDevices)),
-        );
-      },
-    });
+      setLoadedDevices(
+        SBDevices.transformDeviceStatus(
+          lambdaDevices,
+          SBDevices.getDeviceNames(loadedDevices),
+        ),
+      );
+    },
+  });
 
   useEffect(() => {
     SBDevices.getDevices()
       .then((data) => {
         setLoadedDevices(data);
-        setIsLoading(false);
       })
       .catch((err) => {
         console.log(err);
       });
   }, []);
 
-  if (isLoading) {
-    return <div>Loading...</div>;
-  }
-
-  return (
-    <div className="pt-4 px-4 grid gap-4 grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6">
-      {loadedDevices?.map((temp: DeviceStatus, idx: number) => (
-        <TempWidget key={idx} data={temp} prefs={prefs}></TempWidget>
-      ))}
-    </div>
+    return (
+    <>
+      <Suspense fallback={<Loading />}>
+        <div className="pt-4 px-4 grid gap-4 grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6">
+          {loadedDevices?.map((temp: DeviceStatus, idx: number) => (
+            <TempWidget key={idx} data={temp} prefs={prefs}></TempWidget>
+          ))}
+        </div>
+      </Suspense>
+    </>
   );
 }
