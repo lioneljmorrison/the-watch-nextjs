f64e48146b27f2b9d392ccbae0100bc26d6009b4
Lionel <lionel@morrison.mobi>
2023-08-28 09:27:42 -0400

    Implmented Vercel Analytics

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 import MainNav from './components/nav';
 import TempGroupWidget from './components/tempGroup';
 import { navData, prefs } from './data';
+import { Analytics } from '@vercel/analytics/react';
+
 
 export default function Home() {
   const urls = {
@@ -12,6 +14,7 @@ export default function Home() {
     <>
       <MainNav data={navData} name="Hardwick Cider Company"></MainNav>
       <TempGroupWidget urls={urls} prefs={prefs}></TempGroupWidget>
+      <Analytics />
     </>
   );
 }
