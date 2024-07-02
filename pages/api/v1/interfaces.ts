d8f91dd2f9b7455e25940c63ee97249546624313
Lionel Morrison <lionel@morrison.mobi>
2024-07-01 23:35:40 -0400

    Get All Devices from SwitchBot Account

---
+++ pages/api/v1/interfaces.ts
@@ -0,0 +1,78 @@
+export interface Account {
+    company: string;
+    id: string;
+    created: string;
+}
+
+export interface DeviceListStatus {
+    deviceId: string;
+    deviceName: string;
+    deviceType: string;
+    enableCloudService: boolean;
+    hubDeviceId: string;
+}
+
+export interface DeviceImport {
+    deviceId: string;
+    deviceName: string;
+    deviceType: string;
+}
+
+export interface Device extends DeviceListStatus {
+    deviceId: string;
+    accountId: string;
+    created: string;
+}
+
+export interface DeviceI extends DeviceImport {
+    accountId: string;
+    created: string;
+}
+
+export interface DeviceHookResponse {
+    eventType: string;
+    eventVersion: string;
+    context: {
+        deviceType: string;
+        deviceMac: string;
+        temperature: number;
+        humidity: number;
+        battery: number;
+        scale: 'CELSIUS' | 'FAHRENHEIT';
+        timeOfSample: number;
+    };
+}
+
+export interface DeviceList {
+    statusCode: number;
+    body: {
+        deviceList: DeviceListStatus[];
+    };
+    infraredRemoteList: any[];
+    message: string;
+}
+
+export interface DeviceStatus {
+    deviceId: string;
+    deviceType: string;
+    hubDeviceId?: string;
+    humidity: number;
+    temperature: number;
+    version?: string;
+    battery: number;
+}
+
+export interface LogDeviceStatus extends DeviceStatus {
+    created: string;
+    accountId: string;
+}
+
+export interface OpenAPIConfig {
+    secret: string;
+    token: string;
+    nonce: string;
+}
+
+export interface deviceStatusParameters {
+    device: string;
+}
---
+++ pages/switchbot.ts
@@ -0,0 +1,48 @@
+import { DeviceList, DeviceListStatus } from "./api/v1/interfaces";
+import * as crypto from 'crypto-js';
+
+export class SwitchBot {
+    private _token: string;
+    private _secret: string;
+    private _signature: string;
+    private _nonce: string;
+    private _timestamp: string;
+    private _uri = 'https://api.switch-bot.com/v1.1';
+    private _deviceList: unknown;
+
+    constructor(token: string, secret: string) {
+        this._token = token;
+        this._secret = secret;
+        this._nonce = '';
+        this._timestamp = Date.now.toString();
+        this._signature = crypto.HmacSHA256(this._token + this._timestamp + this._nonce, this._secret).toString(crypto.enc.Base64);
+    }
+
+    get signature(): string {
+        return this._signature;
+    }
+
+    get devices(): unknown {
+        return this._deviceList;
+    }
+
+    async getDevices(): Promise<DeviceListStatus[] | string> {
+        const requestOptions: RequestInit = {
+            method: 'GET',
+            headers: {
+                Authorization: this._token,
+                nonce: this._nonce,
+                t: this._timestamp,
+                sign: this._signature,
+            },
+            redirect: 'follow',
+        };
+
+        const result = await fetch(`${this._uri}/devices`, requestOptions)
+        if (result.status !== 200) {
+            return result.statusText;
+        }
+
+        return (JSON.parse(await result.text()) as DeviceList).body.deviceList;
+    }
+}
