7e97da2f0ac4a4362b3c92f6cf9454aae9b37f54
Lionel Morrison <lionel@morrison.mobi>
2025-09-20 20:34:27 -0400

    Not entirly sure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
+'use client';
+
 import MainNav from './components/nav';
+import Task from './components/Tasks/Task';
 import TempGroupWidget from './components/tempGroup';
 import { navData, prefs } from './data';
 import { Analytics } from '@vercel/analytics/react';
-
 
 export default function Home() {
   const urls = {
@@ -10,9 +12,25 @@ export default function Home() {
     lambda: 'https://28a5luoh60.execute-api.us-east-2.amazonaws.com/Prod',
   };
 
+  const onArchiveTask = (id: string) => {
+    console.log('Archive Task', id);
+  },
+  onPinTask = (id: string) => {
+    console.log('Pin Task', id);
+  };
+
   return (
     <>
       <MainNav data={navData} name="Hardwick Cider Company"></MainNav>
+
+
+      <Task task={{ id: '1', title: 'Test Task', state: 'TASK_INBOX'}} onArchiveTask={onArchiveTask} onPinTask={onPinTask} />
+      <Task task={{ id: '1', title: 'Test Task', state: 'TASK_INBOX'}} onArchiveTask={onArchiveTask} onPinTask={onPinTask} />
+      <Task task={{ id: '1', title: 'Test Task', state: 'TASK_INBOX'}} onArchiveTask={onArchiveTask} onPinTask={onPinTask} />
+      <Task task={{ id: '1', title: 'Test Task', state: 'TASK_INBOX'}} onArchiveTask={onArchiveTask} onPinTask={onPinTask} />
+      <Task task={{ id: '1', title: 'Test Task', state: 'TASK_INBOX'}} onArchiveTask={onArchiveTask} onPinTask={onPinTask} />
+      <Task task={{ id: '1', title: 'Test Task', state: 'TASK_INBOX'}} onArchiveTask={onArchiveTask} onPinTask={onPinTask} />
+
       <TempGroupWidget urls={urls} prefs={prefs}></TempGroupWidget>
       <Analytics />
     </>
